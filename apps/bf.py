c8a8ed16bb88f1fe2c562b2d222e06d0ef4610f1
Chandler May <chandler.j.may@gmail.com>
2014-01-13 17:17:09 -0500

    crappy bf w/ serialize

--- apps/bf.py
+++ apps/bf.py
@@ -1,19 +1,29 @@
 import pylowl
 
-def main(filename):
+def main(data_filename, io_flag, bf_filename):
+    if io_flag not in ('-i', '-o'):
+        raise Exception('Invalid io flag')
+
     print('Constructing bloom filter...')
     bf = pylowl.BloomFilter()
     print('Initializing bloom filter...')
-    bf.init(1024, 32)
-    print('Inserting data into bloom filter...')
-    ngrams = dict()
-    with open(filename) as f:
-        for line in f:
-            ngram = ' '.join(line.split()[:-1])
-            ngrams[ngram] = len(ngrams)
-            bf.insert(ngrams[ngram])
+    bf.init(1024 * 1024, 32)
+    if io_flag == '-i':
+        print('Reading bloom filter...')
+        bf.read(bf_filename)
+    else:
+        print('Inserting data into bloom filter...')
+        ngrams = dict()
+        with open(data_filename) as f:
+            for line in f:
+                ngram = ' '.join(line.split()[:-1])
+                ngrams[ngram] = len(ngrams)
+                bf.insert(ngrams[ngram])
     print('Printing bloom filter...')
     bf.cPrint()
+    if io_flag == '-o':
+        print('Writing bloom filter...')
+        bf.write(bf_filename)
     print('Leaving scope...')
 
 if __name__ == '__main__':
