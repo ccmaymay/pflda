6e3be06b072a87dcbcff0e1326319e2bbe52cb61
Chandler May <chandler.j.may@gmail.com>
2014-01-13 16:58:08 -0500

    insert ngrams in bf

--- apps/bf.py
+++ apps/bf.py
@@ -1,12 +1,17 @@
 import pylowl
 
-def main():
+def main(filename):
     print('Constructing bloom filter...')
     bf = pylowl.BloomFilter()
     print('Initializing bloom filter...')
-    bf.init(1024, 64)
+    bf.init(1024, 32)
     print('Inserting data into bloom filter...')
-    bf.insert(42)
+    ngrams = dict()
+    with open(filename) as f:
+        for line in f:
+            ngram = ' '.join(line.split()[:-1])
+            ngrams[ngram] = len(ngrams)
+            bf.insert(ngrams[ngram])
     print('Printing bloom filter...')
     bf.cPrint()
     print('Leaving scope...')
