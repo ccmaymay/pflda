9a6927f15853fcc2e15fdc2e13b31689e9178ec6
Chandler May <chandler.j.may@gmail.com>
2014-01-13 16:18:42 -0500

    change cython to use new lowl fn names

--- core/lowl.pxd
+++ core/lowl.pxd
@@ -7,13 +7,13 @@ cdef extern from "lowl_types.h":
     ctypedef unsigned int lowl_count
 
 cdef extern from "lowl_bloom.h":
-    ctypedef struct lowl_bloomfilter:
+    ctypedef struct bloomfilter:
         pass
 
-    void lowl_bloomfilter_init(lowl_bloomfilter* f, size_t size, unsigned int k)
-    void lowl_bloomfilter_insertKey(lowl_bloomfilter* f, lowl_key k)
-    int  lowl_bloomfilter_queryKey(lowl_bloomfilter* f, lowl_key k)
-    void lowl_bloomfilter_print(lowl_bloomfilter* f)
-    void lowl_bloomfilter_write(lowl_bloomfilter* f, FILE* fp)
-    void lowl_bloomfilter_read(lowl_bloomfilter* f, FILE* fp)
-    void lowl_bloomfilter_destroy(lowl_bloomfilter* f)
+    void bloomfilter_init(bloomfilter* f, size_t size, unsigned int k)
+    void bloomfilter_insertKey(bloomfilter* f, lowl_key k)
+    int  bloomfilter_queryKey(bloomfilter* f, lowl_key k)
+    void bloomfilter_print(bloomfilter* f)
+    void bloomfilter_write(bloomfilter* f, FILE* fp)
+    void bloomfilter_read(bloomfilter* f, FILE* fp)
+    void bloomfilter_destroy(bloomfilter* f)
--- core/pylowl.pyx
+++ core/pylowl.pyx
@@ -2,32 +2,32 @@ cimport lowl
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
 cdef class BloomFilter:
-    cdef lowl.lowl_bloomfilter* _bf
+    cdef lowl.bloomfilter* _bf
 
     def __cinit__(self):
-        self._bf = <lowl.lowl_bloomfilter *>PyMem_Malloc(sizeof(lowl.lowl_bloomfilter))
+        self._bf = <lowl.bloomfilter *>PyMem_Malloc(sizeof(lowl.bloomfilter))
         if self._bf is NULL:
             raise MemoryError()
 
     def init(self, size, k):
-        lowl.lowl_bloomfilter_init(self._bf, size, k)
+        lowl.bloomfilter_init(self._bf, size, k)
 
     def insert(self, k):
-        lowl.lowl_bloomfilter_insertKey(self._bf, k)
+        lowl.bloomfilter_insertKey(self._bf, k)
 
     def query(self, k):
-        lowl.lowl_bloomfilter_queryKey(self._bf, k)
+        lowl.bloomfilter_queryKey(self._bf, k)
 
     def cPrint(self):
-        lowl.lowl_bloomfilter_print(self._bf)
+        lowl.bloomfilter_print(self._bf)
 
     #def read(self, fp):
-    #    lowl.lowl_bloomfilter_read(self._bf, fp)
+    #    lowl.bloomfilter_read(self._bf, fp)
 
     #def write(self, fp):
-    #    lowl.lowl_bloomfilter_write(self._bf, fp)
+    #    lowl.bloomfilter_write(self._bf, fp)
 
     def __dealloc__(self):
         if self._bf is not NULL:
-            lowl.lowl_bloomfilter_destroy(self._bf)
+            lowl.bloomfilter_destroy(self._bf)
             PyMem_Free(self._bf)
