b8314cbaf4f48704cf2156ca631143eb0678c95a
Keith Levin <keith.levin@gmail.com>
2014-01-13 18:21:08 -0500

    Merged lowl_bloom into lowl_sketch. lowl_sketch is now for all counting/sketching algorithms. Added error codes to lowl_sketch, though they are not yet informative (i.e., every error returns -1).

--- core/lowl_sketch.h
+++ core/lowl_sketch.h
@@ -1,6 +1,8 @@
 #ifndef LOWLSKETCH_H
 #define LOWLSKETCH_H
 
+#include <stdint.h>
+
 /* functions and structures related to counting and sketching algorithms. */
 
 typedef struct cmsketch{
@@ -15,13 +17,31 @@ typedef struct cmsketch{
 
 int cmsketch_init( cmsketch* cm, unsigned int m,
 			unsigned int w, unsigned int d);
-
 int cmsketch_update( cmsketch* cm, unsigned int i, unsigned int c );
-
 int cmsketch_count( cmsketch* cm, unsigned int token );
-
 void cmsketch_clear( cmsketch* cm );
-
 void cmsketch_destroy( cmsketch* cm );
+
+
+typedef struct bloomfilter { 
+  unsigned int size;
+  unsigned int k; /* number of hash functions to use. */
+  uint32_t* b;
+  lowl_key_hash* hash_key_to_word;
+  lowl_key_hash* hash_key_to_bit;
+  uint32_t* mask;
+}bloomfilter;
+
+// Self-explanatory functions
+int bloomfilter_init(bloomfilter* f, size_t size, unsigned int k);
+void bloomfilter_setmask( uint32_t* mask );
+void bloomfilter_insertKey(bloomfilter* f, lowl_key k);
+void bloomfilter_insertString(bloomfilter* f, char* x, int len);
+int  bloomfilter_queryKey(bloomfilter* f, lowl_key k);
+int  bloomfilter_queryString(bloomfilter* f, char* x, int len);
+void bloomfilter_print(bloomfilter* f);
+void bloomfilter_write(bloomfilter* f, FILE* fp);
+void bloomfilter_read(bloomfilter* f, FILE* fp);
+void bloomfilter_destroy(bloomfilter* f);
 
 #endif
