4d2f683a155fc5394cf8906f6b3186852886b1e4
Chandler May <cjmay4754@gmail.com>
2014-01-14 17:34:54 -0500

    update bf driver to use char filter

--- apps/bf.py
+++ apps/bf.py
@@ -13,34 +13,22 @@ def main(cmd, *args):
         data_filename = args[0]
         bf_filename = args[1]
         print('Inserting data into bloom filter...')
-        ngrams = dict()
         with open(data_filename) as f:
             for line in f:
                 ngram = ' '.join(line.split()[:-1])
-                ngrams[ngram] = len(ngrams)
-                bf.insert(ngrams[ngram])
+                bf.insert(ngram)
         print('Writing bloom filter...')
         bf.write(bf_filename)
         ## bvd: the following spews bits to STDOUT
         #print('Printing bloom filter...')
         #bf.cPrint()
     else:
-        data_filename = args[0]
-        bf_filename = args[1]
-        query = ' '.join(args[2:])
-        print('Loading dictionary...')
-        ngrams = dict()
-        with open(data_filename) as f:
-            for line in f:
-                ngram = ' '.join(line.split()[:-1])
-                ngrams[ngram] = len(ngrams)
+        bf_filename = args[0]
+        ngram = args[1]
         print('Reading bloom filter...')
         bf.read(bf_filename)
         print('Querying bloom filter...')
-        if query not in ngrams:
-            ngrams[query] = len(ngrams)
-        key = ngrams[query]
-        print(bf.query(key))
+        print(bf.query(ngram))
     print('Leaving scope...')
 
 if __name__ == '__main__':
