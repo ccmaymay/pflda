db2de15e2f6cbcdde9a6595e3a1dcd4c03aa04a6
Chandler May <cjmay4754@gmail.com>
2014-01-14 18:28:02 -0500

    reservoir sampler py work in progress

--- core/lowl_sample.c
+++ core/lowl_sample.c
@@ -17,26 +17,27 @@ int reservoirsampler_init(reservoirsampler* rs, size_t capacity) {
 
 void reservoirsampler_destroy(reservoirsampler* rs) {
   free(rs->sample);
+  rs->sample = NULL;
 }
 
-int reservoirsampler_add(reservoirsampler* rs, lowl_key x, size_t *idx,
+bool reservoirsampler_insert(reservoirsampler* rs, lowl_key x, size_t *idx,
     lowl_key *ejected) {
-  if (rs->occupied < rs->capacity) {
-    *idx = rs->occupied;
+  if (rs->stream_pos < rs->capacity) {
+    *idx = rs->stream_pos;
     ejected = 0;
     rs->sample[*idx] = x;
-    ++(rs->occupied);
-    return 0;
+    ++(rs->stream_pos);
+    return true;
   } else {
     *idx = random() % rs->stream_pos; // TODO not uniform
     if (*idx < rs->capacity) {
       *ejected = rs->sample[*idx];
       rs->sample[*idx] = x;
-      return 0;
+      return true;
     } else {
       *idx = rs->capacity + 1;
       ejected = 0;
-      return 1;
+      return false;
     }
   }
 }
@@ -46,5 +47,45 @@ size_t reservoirsampler_capacity(reservoirsampler* rs) {
 }
 
 size_t reservoirsampler_occupied(reservoirsampler* rs) {
-  return min(rs->occupied, rs->capacity);
+  return min(rs->stream_pos, rs->capacity);
+}
+
+void reservoirsampler_print(reservoirsampler* rs) {
+  printf("%u", rs->sample[0]);
+  size_t occupied = reservoirsampler_occupied(rs);
+  for (size_t i = 1; i < occupied; ++i)
+    printf(" %u", rs->sample[i]);
+  for (size_t i = occupied; i < rs->capacity; ++i)
+    printf(" ()");
+  printf("\n");
+}
+
+void reservoirsampler_write(reservoirsampler* rs, FILE* fp) {
+  fwrite( &(f->capacity), sizeof(size_t), 1, fp);
+  fwrite( &(f->stream_pos), sizeof(size_t), 1, fp);
+  fwrite( f->sample, sizeof( lowl_key ), f->size, fp);
+  fwrite( &(f->hash_key_to_word1), sizeof( char_hash ), 1, fp);
+  fwrite( &(f->hash_key_to_word2), sizeof( char_hash ), 1, fp);
+  fwrite( &(f->hash_key_to_bit1), sizeof( char_hash ), 1, fp); 
+  fwrite( &(f->hash_key_to_bit2), sizeof( char_hash ), 1, fp); 
+}
+
+void reservoirsampler_read(reservoirsampler* rs, FILE* fp) {
+}
+
+void bloomfilter_write(bloomfilter* f, FILE* fp) {
+}
+
+void bloomfilter_read(bloomfilter* f, FILE* fp) {
+  f->mask = (uint32_t*) malloc(32 * sizeof(uint32_t));
+  bloomfilter_setmask( f->mask );
+
+  fread(&(f->size), sizeof(int), 1, fp);
+  fread(&(f->k), sizeof(int), 1, fp);
+  f->b = (uint32_t*)malloc( f->size*sizeof(uint32_t));
+  fread(f->b, sizeof(uint32_t), f->size, fp);
+  fread( &(f->hash_key_to_word1), sizeof( char_hash ), 1, fp);
+  fread( &(f->hash_key_to_word2), sizeof( char_hash ), 1, fp);
+  fread( &(f->hash_key_to_bit1), sizeof( char_hash ), 1, fp);
+  fread( &(f->hash_key_to_bit2), sizeof( char_hash ), 1, fp);
 }
--- core/pylowl.pyx
+++ core/pylowl.pyx
@@ -1,6 +1,7 @@
 cimport lowl
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
+
 cdef class BloomFilter:
     cdef lowl.bloomfilter* _bf
 
@@ -11,6 +12,7 @@ cdef class BloomFilter:
 
     def init(self, size, k):
         lowl.bloomfilter_init(self._bf, size, k)
+        # TODO error code
 
     def insert(self, x):
         lowl.bloomfilter_insert(self._bf, x, len(x))
@@ -35,3 +37,46 @@ cdef class BloomFilter:
         if self._bf is not NULL:
             lowl.bloomfilter_destroy(self._bf)
             PyMem_Free(self._bf)
+
+
+cdef class ReservoirSampler:
+    cdef lowl.reservoirsampler* _rs
+
+    def __cinit__(self):
+        self._rs = <lowl.reservoirsampler *>PyMem_Malloc(sizeof(lowl.reservoirsampler))
+        if self._rs is NULL:
+            raise MemoryError()
+
+    def init(self, capacity):
+        lowl.reservoirsampler_init(self._rs, capacity)
+        # TODO error code
+
+    def insert(self, k):
+        cdef size_t idx
+        cdef lowl.lowl_key ejected
+        inserted = lowl.reservoirsampler_insert(self._rs, k, idx, ejected)
+        return (inserted, idx, ejected) # TODO
+
+    def read(self, filename):
+        f = lowl.fopen(filename, 'rb')
+        lowl.reservoirsampler_read(self._rs, f)
+        lowl.fclose(f)
+
+    def write(self, filename):
+        f = lowl.fopen(filename, 'wb')
+        lowl.reservoirsampler_write(self._rs, f)
+        lowl.fclose(f)
+
+    def capacity(self):
+        return lowl.reservoirsampler_capacity(self._rs)
+
+    def occupied(self):
+        return lowl.reservoirsampler_occupied(self._rs)
+
+    def cPrint(self):
+        lowl.reservoirsampler_print(self._rs)
+
+    def __dealloc__(self):
+        if self._rs is not NULL:
+            lowl.reservoirsampler_destroy(self._rs)
+            PyMem_Free(self._rs)
