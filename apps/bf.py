aec9025749ee6321efad964fb95ed701faefebcd
Chandler May <cjmay4754@gmail.com>
2014-01-17 12:34:35 -0500

    patch bf.py wrt new bloomfilter query/insert signatures, credit Adam Lopez

--- apps/bf.py
+++ apps/bf.py
@@ -20,7 +20,7 @@ def main(cmd, *args):
         with open(data_filename) as f:
             for line in f:
                 ngram = ' '.join(line.split()[:-1])
-                bf.insert(ngram)
+                bf.insert(ngram, len(ngram))
         print('Writing bloom filter...')
         bf.write(bf_filename)
         ## bvd: the following spews bits to STDOUT
@@ -32,7 +32,7 @@ def main(cmd, *args):
         print('Reading bloom filter...')
         bf.read(bf_filename)
         print('Querying bloom filter...')
-        print(bf.query(ngram))
+        print(bf.query(ngram, len(ngram)))
     print('Leaving scope...')
 
 if __name__ == '__main__':
