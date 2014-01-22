59d9c65af00bca7076dbc7a524dab8d6cc1ef636
Chandler May <chandler.j.may@gmail.com>
2014-01-22 18:46:38 -0500

    cythonic lda

--- apps/lda/lda.pyx
+++ apps/lda/lda.pyx
@@ -1,73 +1,89 @@
 #!/usr/bin/env python
 
 
-import pylowl
+import pylowl # TODO cimport?
 import random
-import re
 import util
 import sys
+import numpy
+cimport numpy
 
 
-class LdaModel(object):
-    def __init__(self, reservoir_size, alpha, beta, num_topics, vocab):
+cdef class LdaModel(object):
+    cdef numpy.double_t alpha, beta
+    cdef numpy.uint_t num_topics, num_docs, vocab_size
+    cdef numpy.uint_t[:, ::1] tw_counts, dt_counts
+    cdef numpy.uint_t[:] t_counts, d_counts
+    cdef numpy.double_t[:] pmf
+
+    def __cinit__(self, numpy.double_t alpha, numpy.double_t beta, numpy.uint_t num_docs, numpy.uint_t num_topics, numpy.uint_t vocab_size):
         self.alpha = alpha
         self.beta = beta
         self.num_topics = num_topics
-        self.vocab = vocab
-        self.tw_counts = None
-        self.t_counts = None
-        self.dt_counts = None
-        self.d_counts = None
-        self.reservoir = pylowl.ValuedReservoirSampler(reservoir_size)
+        self.num_docs = num_docs
+        self.vocab_size = vocab_size
+        self.tw_counts = numpy.zeros((num_topics, vocab_size), dtype=numpy.uint)
+        self.t_counts = numpy.zeros((num_topics,), dtype=numpy.uint)
+        self.dt_counts = numpy.zeros((num_docs, num_topics), dtype=numpy.uint)
+        self.d_counts = numpy.zeros((num_docs,), dtype=numpy.uint)
+        self.pmf = numpy.zeros((num_topics,), dtype=numpy.double)
 
-    def conditional_posterior(self, i, w, t):
-        return (self.tw_counts[t * len(self.vocab) + w] + self.beta) / float(self.t_counts[t] + len(self.vocab) * self.beta) * (self.dt_counts[i * self.num_topics + t] + self.alpha) / float(self.d_counts[i] + self.num_topics * self.alpha)
+    cdef numpy.double_t conditional_posterior(self, numpy.uint_t i, numpy.uint_t w, numpy.uint_t t):
+        return (self.tw_counts[t, w] + self.beta) / (self.t_counts[t] + self.vocab_size * self.beta) * (self.dt_counts[i, t] + self.alpha) / (self.d_counts[i] + self.num_topics * self.alpha)
 
-    def sample_topic(self, i, w):
-        dist = [self.conditional_posterior(i, w, t) for t in range(self.num_topics)]
-        r = random.random() * sum(dist)
-        for k in range(len(dist)-1):
-            if r < dist[k]:
-                return k
-            dist[k+1] += dist[k]
-        return len(dist) - 1
+    cdef numpy.uint_t sample_topic(self, numpy.uint_t i, numpy.uint_t w):
+        cdef numpy.double_t prior, r
+        cdef numpy.uint_t t
 
-    def preprocess(self, doc_triple):
-        return [self.vocab[w] for w in doc_triple[2]]
+        prior = 0.0
+        for t in xrange(self.num_topics):
+            self.pmf[t] = self.conditional_posterior(i, w, t)
+            prior += self.pmf[t]
 
-    def add_doc(self, doc_triple):
-        self.reservoir.insert(doc_triple, self.preprocess)
+        r = random.random() * prior
+        for t in xrange(self.num_topics-1):
+            if r < self.pmf[t]:
+                return t
+            self.pmf[t+1] += self.pmf[t]
 
-    def learn(self, num_iters):
-        sample = self.reservoir.sample()
+        return self.num_topics - 1
+
+    cdef learn(self, list sample, numpy.uint_t num_iters):
+        cdef list assignments
+        cdef numpy.uint_t t, i, j, w, m, z
+
         assignments = []
-        self.tw_counts = [0] * (self.num_topics * len(self.vocab))
-        self.t_counts = [0] * self.num_topics
-        self.dt_counts = [0] * (len(sample) * self.num_topics)
-        self.d_counts = [0] * len(sample)
-        for i in range(len(sample)):
-            for j in range(len(sample[i])):
+        # TODO
+        #self.tw_counts = [0] * (self.num_topics * self.vocab_size)
+        #self.t_counts = [0] * self.num_topics
+        #self.dt_counts = [0] * (self.num_docs * self.num_topics)
+        #self.d_counts = [0] * self.num_docs
+
+        for i in xrange(self.num_docs):
+            for j in xrange(len(sample[i])):
                 w = sample[i][j]
-                assignments.append(random.randint(0, self.num_topics - 1))
-                self.tw_counts[assignments[-1] * len(self.vocab) + w] += 1
-                self.t_counts[assignments[-1]] += 1
-                self.dt_counts[i * self.num_topics + assignments[-1]] += 1
+                z = random.randint(0, self.num_topics - 1)
+                assignments.append(z)
+                self.tw_counts[z, w] += 1
+                self.t_counts[z] += 1
+                self.dt_counts[i, z] += 1
                 self.d_counts[i] += 1
-        for t in range(num_iters):
+
+        for t in xrange(num_iters):
             m = 0
-            for i in range(len(sample)):
-                for j in range(len(sample[i])):
+            for i in xrange(self.num_docs):
+                for j in xrange(len(sample[i])):
                     w = sample[i][j]
                     z = assignments[m]
-                    self.tw_counts[z * len(self.vocab) + w] -= 1
+                    self.tw_counts[z, w] -= 1
                     self.t_counts[z] -= 1
-                    self.dt_counts[i * self.num_topics + z] -= 1
+                    self.dt_counts[i, z] -= 1
                     self.d_counts[i] -= 1
-                    assignments[m] = self.sample_topic(i, w)
-                    z = assignments[m]
-                    self.tw_counts[z * len(self.vocab) + w] += 1
+                    z = self.sample_topic(i, w)
+                    assignments[m] = z
+                    self.tw_counts[z, w] += 1
                     self.t_counts[z] += 1
-                    self.dt_counts[i * self.num_topics + z] += 1
+                    self.dt_counts[i, z] += 1
                     self.d_counts[i] += 1
                     m += 1
             sys.stdout.write('.')
@@ -75,11 +91,11 @@ class LdaModel(object):
         sys.stdout.write('\n')
         sys.stdout.flush()
 
-    def __str__(self):
+    def to_string(self, vocab):
         s = ''
         for t in range(self.num_topics):
             s += 'TOPIC %d:\n' % t
-            pp = [(word, self.tw_counts[t * len(self.vocab) + self.vocab[word]]) for word in self.vocab]
+            pp = [(word, self.tw_counts[t, w]) for (word, w) in vocab.items()]
             pp.sort(key=lambda p: p[1], reverse=True)
             for (word, count) in pp:
                 if count > 0:
@@ -88,14 +104,25 @@ class LdaModel(object):
 
 
 def run_lda(data_dir, *categories):
+    reservoir_size = 1000
+    num_iters = 1000
+    num_topics = 3
+    alpha = 0.1
+    beta = 0.1
+
+    reservoir = pylowl.ValuedReservoirSampler(reservoir_size)
     dataset = util.Dataset(data_dir, set(categories))
-    model = LdaModel(1000, 0.1, 0.1, 3, dataset.vocab)
+    model = LdaModel(alpha, beta, reservoir_size, num_topics, len(dataset.vocab))
+
+    def preprocess(doc_triple):
+        return [dataset.vocab[w] for w in doc_triple[2]]
+
     i = 0
     for doc_triple in dataset.train_iterator():
-        if i > 0 and i % 100 == 0:
-            model.learn(1000)
-            print(model)
-        model.add_doc(doc_triple)
+        if i >= reservoir_size and i % 100 == 0:
+            model.learn(reservoir.sample(), num_iters)
+            print(model.to_string(dataset.vocab))
+        reservoir.insert(doc_triple, preprocess)
         i += 1
 
 
