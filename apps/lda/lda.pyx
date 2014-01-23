3940be96cdee286f3eb146e3c966025a1f4a03af
Chandler May <chandler.j.may@gmail.com>
2014-01-23 12:26:25 -0500

    separate lda model out of gibbs sampler

--- apps/lda/lda.pyx
+++ apps/lda/lda.pyx
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-import pylowl # TODO cimport?
+import pylowl
 import random
 import util
 import sys
@@ -10,24 +10,31 @@ cimport numpy
 from cpython.exc cimport PyErr_CheckSignals
 
 
-cdef class LdaModel(object):
+cdef class GlobalParams:
     cdef numpy.double_t alpha, beta
-    cdef numpy.uint_t num_topics, num_docs, vocab_size
-    cdef numpy.uint_t[:, ::1] tw_counts, dt_counts
-    cdef numpy.uint_t[:] t_counts, d_counts
-    cdef numpy.double_t[:] pmf
+    cdef numpy.uint_t num_topics, vocab_size
+    cdef numpy.uint_t[:, ::1] tw_counts
+    cdef numpy.uint_t[:] t_counts
 
-    def __cinit__(self, numpy.double_t alpha, numpy.double_t beta, numpy.uint_t num_docs, numpy.uint_t num_topics, numpy.uint_t vocab_size):
+    def __cinit__(self, numpy.double_t alpha, numpy.double_t beta,
+            numpy.uint_t num_topics, numpy.uint_t vocab_size):
         self.alpha = alpha
         self.beta = beta
         self.num_topics = num_topics
-        self.num_docs = num_docs
         self.vocab_size = vocab_size
         self.tw_counts = numpy.zeros((num_topics, vocab_size), dtype=numpy.uint)
         self.t_counts = numpy.zeros((num_topics,), dtype=numpy.uint)
-        self.dt_counts = numpy.zeros((num_docs, num_topics), dtype=numpy.uint)
-        self.d_counts = numpy.zeros((num_docs,), dtype=numpy.uint)
-        self.pmf = numpy.zeros((num_topics,), dtype=numpy.double)
+        
+
+cdef class GibbsSampler:
+    cdef GlobalParams global_params
+    cdef numpy.uint_t[:, ::1] dt_counts
+    cdef numpy.uint_t[:] d_counts
+    cdef numpy.double_t[:] pmf
+
+    def __cinit__(self, GlobalParams global_params):
+        self.global_params = global_params
+        self.pmf = numpy.zeros((global_params.num_topics,), dtype=numpy.double)
 
     cdef eval_pl(self, list sample):
         cdef numpy.double_t local_d_count, ll, s, p
@@ -37,17 +44,17 @@ cdef class LdaModel(object):
         num_words = 0
         ll = 0.0
         local_d_count = 0.0
-        local_dt_counts = numpy.zeros((self.num_topics,), dtype=numpy.double)
-        x = numpy.zeros((self.num_topics,), dtype=numpy.double)
+        local_dt_counts = numpy.zeros((self.global_params.num_topics,), dtype=numpy.double)
+        x = numpy.zeros((self.global_params.num_topics,), dtype=numpy.double)
         for i in xrange(len(sample)):
             local_dt_counts[:] = 0.0
             for j in xrange(len(sample[i])):
                 w = sample[i][j]
-                for t in xrange(self.num_topics):
-                    x[t] = (self.tw_counts[t, w] + self.beta) / (self.t_counts[t] + self.vocab_size * self.beta) * (local_dt_counts[t] + self.alpha) / (local_d_count + self.num_topics * self.alpha)
+                for t in xrange(self.global_params.num_topics):
+                    x[t] = (self.global_params.tw_counts[t, w] + self.global_params.beta) / (self.global_params.t_counts[t] + self.global_params.vocab_size * self.global_params.beta) * (local_dt_counts[t] + self.global_params.alpha) / (local_d_count + self.global_params.num_topics * self.global_params.alpha)
                 s = numpy.sum(x)
                 ll += numpy.log(s)
-                for t in xrange(self.num_topics):
+                for t in xrange(self.global_params.num_topics):
                     p = x[t] / s
                     local_dt_counts[t] += p
                     local_d_count += p
@@ -59,42 +66,42 @@ cdef class LdaModel(object):
         print('Perplexity:     %f' % (-ll / num_words))
 
     cdef numpy.double_t conditional_posterior(self, numpy.uint_t i, numpy.uint_t w, numpy.uint_t t):
-        return (self.tw_counts[t, w] + self.beta) / (self.t_counts[t] + self.vocab_size * self.beta) * (self.dt_counts[i, t] + self.alpha) / (self.d_counts[i] + self.num_topics * self.alpha)
+        return (self.global_params.tw_counts[t, w] + self.global_params.beta) / (self.global_params.t_counts[t] + self.global_params.vocab_size * self.global_params.beta) * (self.dt_counts[i, t] + self.global_params.alpha) / (self.d_counts[i] + self.global_params.num_topics * self.global_params.alpha)
 
     cdef numpy.uint_t sample_topic(self, numpy.uint_t i, numpy.uint_t w):
         cdef numpy.double_t prior, r
         cdef numpy.uint_t t
 
         prior = 0.0
-        for t in xrange(self.num_topics):
+        for t in xrange(self.global_params.num_topics):
             self.pmf[t] = self.conditional_posterior(i, w, t)
             prior += self.pmf[t]
 
         r = random.random() * prior
-        for t in xrange(self.num_topics-1):
+        for t in xrange(self.global_params.num_topics-1):
             if r < self.pmf[t]:
                 return t
             self.pmf[t+1] += self.pmf[t]
 
-        return self.num_topics - 1
+        return self.global_params.num_topics - 1
 
     cdef learn(self, list sample, numpy.uint_t num_iters):
         cdef list assignments
-        cdef numpy.uint_t t, i, j, w, m, z
+        cdef numpy.uint_t t, i, j, w, m, z, num_docs
+
+        num_docs = len(sample)
 
         assignments = []
-        self.tw_counts[:] = 0
-        self.t_counts[:] = 0
-        self.dt_counts[:] = 0
-        self.d_counts[:] = 0
+        self.dt_counts = numpy.zeros((num_docs, self.global_params.num_topics), dtype=numpy.uint)
+        self.d_counts = numpy.zeros((num_docs,), dtype=numpy.uint)
 
-        for i in xrange(len(sample)):
+        for i in xrange(num_docs):
             for j in xrange(len(sample[i])):
                 w = sample[i][j]
-                z = random.randint(0, self.num_topics - 1)
+                z = random.randint(0, self.global_params.num_topics - 1)
                 assignments.append(z)
-                self.tw_counts[z, w] += 1
-                self.t_counts[z] += 1
+                self.global_params.tw_counts[z, w] += 1
+                self.global_params.t_counts[z] += 1
                 self.dt_counts[i, z] += 1
                 self.d_counts[i] += 1
             if i % 1000 == 0:
@@ -102,18 +109,18 @@ cdef class LdaModel(object):
 
         for t in xrange(num_iters):
             m = 0
-            for i in xrange(len(sample)):
+            for i in xrange(num_docs):
                 for j in xrange(len(sample[i])):
                     w = sample[i][j]
                     z = assignments[m]
-                    self.tw_counts[z, w] -= 1
-                    self.t_counts[z] -= 1
+                    self.global_params.tw_counts[z, w] -= 1
+                    self.global_params.t_counts[z] -= 1
                     self.dt_counts[i, z] -= 1
                     self.d_counts[i] -= 1
                     z = self.sample_topic(i, w)
                     assignments[m] = z
-                    self.tw_counts[z, w] += 1
-                    self.t_counts[z] += 1
+                    self.global_params.tw_counts[z, w] += 1
+                    self.global_params.t_counts[z] += 1
                     self.dt_counts[i, z] += 1
                     self.d_counts[i] += 1
                     m += 1
@@ -126,9 +133,9 @@ cdef class LdaModel(object):
 
     def to_string(self, vocab, num_words_per_topic):
         s = ''
-        for t in range(self.num_topics):
+        for t in range(self.global_params.num_topics):
             s += 'TOPIC %d:\n' % t
-            pp = [(word, self.tw_counts[t, w]) for (word, w) in vocab.items()]
+            pp = [(word, self.global_params.tw_counts[t, w]) for (word, w) in vocab.items()]
             pp.sort(key=lambda p: p[1], reverse=True)
             i = 0
             for (word, count) in pp:
@@ -147,9 +154,10 @@ def run_lda(data_dir, *categories):
     alpha = 0.1
     beta = 0.1
 
-    reservoir = pylowl.ValuedReservoirSampler(reservoir_size)
     dataset = util.Dataset(data_dir, set(categories))
-    model = LdaModel(alpha, beta, reservoir_size, num_topics, len(dataset.vocab))
+    reservoir = pylowl.ValuedReservoirSampler(reservoir_size)
+    global_params = GlobalParams(alpha, beta, num_topics, len(dataset.vocab))
+    gibbs_sampler = GibbsSampler(global_params)
 
     def preprocess(doc_triple):
         return [dataset.vocab[w] for w in doc_triple[2]]
@@ -159,9 +167,9 @@ def run_lda(data_dir, *categories):
     i = 0
     for doc_triple in dataset.train_iterator():
         if i >= reservoir_size and i % 100 == 0:
-            model.learn(reservoir.sample(), num_iters)
-            print(model.to_string(dataset.vocab, 20))
-            model.eval_pl(test_sample)
+            gibbs_sampler.learn(reservoir.sample(), num_iters)
+            print(gibbs_sampler.to_string(dataset.vocab, 20))
+            gibbs_sampler.eval_pl(test_sample)
         reservoir.insert(doc_triple, preprocess)
         i += 1
 
