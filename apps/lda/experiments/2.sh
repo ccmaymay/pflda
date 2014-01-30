1055e070aeae52aa6e20f9fe8e964545648cd6bd
Chandler May <cjmay@jhu.edu>
2014-01-30 11:30:19 -0500

    change init_num_iters to 2000 for consistency?

--- apps/lda/experiments/2.sh
+++ apps/lda/experiments/2.sh
@@ -7,20 +7,20 @@ bash qsub_tng3.sh 2-rs1k-ibs0 -q text.q -tc 2 \
 bash qsub_tng3.sh 2-rs1k-ibs10 -q text.q -tc 2 \
     -l num_proc=1,mem_free=200M,h_rt=2:00:00 \
     run_lda.qsub $dataset --reservoir_size=1000 \
-    --init_num_docs=10 --init_num_iters=10
+    --init_num_docs=10 --init_num_iters=2000
 bash qsub_tng3.sh 2-rs1k-ibs30 -q text.q -tc 2 \
     -l num_proc=1,mem_free=200M,h_rt=2:00:00 \
     run_lda.qsub $dataset --reservoir_size=1000 \
-    --init_num_docs=30 --init_num_iters=30
+    --init_num_docs=30 --init_num_iters=2000
 bash qsub_tng3.sh 2-rs1k-ibs100 -q text.q -tc 2 \
     -l num_proc=1,mem_free=200M,h_rt=2:00:00 \
     run_lda.qsub $dataset --reservoir_size=1000 \
-    --init_num_docs=100 --init_num_iters=100
+    --init_num_docs=100 --init_num_iters=2000
 bash qsub_tng3.sh 2-rs1k-ibs300 -q text.q -tc 2 \
     -l num_proc=1,mem_free=200M,h_rt=2:00:00 \
     run_lda.qsub $dataset --reservoir_size=1000 \
-    --init_num_docs=300 --init_num_iters=300
+    --init_num_docs=300 --init_num_iters=2000
 bash qsub_tng3.sh 2-rs1k-ibs1k -q text.q -tc 2 \
     -l num_proc=1,mem_free=200M,h_rt=2:00:00 \
     run_lda.qsub $dataset --reservoir_size=1000 \
-    --init_num_docs=1000 --init_num_iters=1000
+    --init_num_docs=1000 --init_num_iters=2000
