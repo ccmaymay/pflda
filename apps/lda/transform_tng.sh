dd5747f8eb89ed36bdc1d81649f97be4a2fa337d
Chandler May <chandler.j.may@gmail.com>
2014-02-09 13:41:35 -0500

    no-header hack for tng

--- apps/lda/transform_tng.sh
+++ apps/lda/transform_tng.sh
@@ -2,3 +2,6 @@
 python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-nonalpha --split_mode=nonalpha --stop_list_path=../../data/txt/stop_list.txt --lower=t
 python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-whitespace --split_mode=whitespace --stop_list_path=../../data/txt/stop_list.txt --lower=t
 python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-whitespace-mc --split_mode=whitespace --stop_list_path=../../data/txt/stop_list.txt
+python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-noheader-nonalpha --split_mode=nonalpha --stop_list_path=../../data/txt/stop_list.txt --lower=t --remove_header=t
+python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-noheader-whitespace --split_mode=whitespace --stop_list_path=../../data/txt/stop_list.txt --lower=t --remove_header=t
+python data.py transform_tng ../../data/txt/20news-bydate-{train,test} ../../data/txt/tng-noheader-whitespace-mc --split_mode=whitespace --stop_list_path=../../data/txt/stop_list.txt --remove_header=t
