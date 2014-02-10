66a487104e2108b0f9836785e608dbc8d3038b72
Chandler May <cjmay@jhu.edu>
2014-02-09 22:37:52 -0500

    add e 12, 13 skeletons

---
+++ apps/lda/experiments/13.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+dataset_path=../../data/txt/tng-whitespace
+for dataset_name in diff3 sim3 rel3
+do
+    if [ $dataset_name == diff3 ]
+    then
+        init_num_docs=167
+        init_num_iters=200
+        init_seed=0
+    elif [ $dataset_name == sim3 ]
+    then
+        init_num_docs=177
+        init_num_iters=200
+        init_seed=0
+    elif [ $dataset_name == rel3 ]
+    then
+        init_num_docs=158
+        init_num_iters=200
+        init_seed=0
+    fi
+
+    #bash qsub.sh $dataset_path $dataset_name 13-rs0 \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+    #    "--reservoir_size=0 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    #bash qsub.sh $dataset_path $dataset_name 13-rs10 \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+    #    "--reservoir_size=10 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    #bash qsub.sh $dataset_path $dataset_name 13-rs100 \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+    #    "--reservoir_size=100 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    bash qsub.sh $dataset_path $dataset_name 13-rs1k \
+        "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+        "--reservoir_size=1000 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    #bash qsub.sh $dataset_path $dataset_name 13-rs10k \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=500M,h_rt=4:00:00" \
+    #    "--reservoir_size=10000 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    #bash qsub.sh $dataset_path $dataset_name 13-rs100k \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=2G,h_rt=12:00:00" \
+    #    "--reservoir_size=100000 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+    #bash qsub.sh $dataset_path $dataset_name 13-rs500k \
+    #    "-q text.q -tc 2 -l num_proc=1,mem_free=4G,h_rt=12:00:00" \
+    #    "--reservoir_size=500000 --init_num_docs=$init_num_docs --init_num_iters=$init_num_iters"
+done
