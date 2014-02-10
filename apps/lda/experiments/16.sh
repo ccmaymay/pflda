994be1487f9956a1862475db27a55d5382a78999
Chandler May <cjmay@jhu.edu>
2014-02-10 09:00:49 -0500

    fix dataset path in e 16

--- apps/lda/experiments/16.sh
+++ apps/lda/experiments/16.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dataset_path=../../data/txt/tng-nonalpha
+dataset_path=../../data/txt/twitter
 bash qsub.sh $dataset_path null 16-rs0 \
     "-q text.q -tc 2 -l num_proc=1,mem_free=1G,h_rt=12:00:00" \
     "--reservoir_size=0 --init_num_docs=500 --init_num_iters=2000 --num_topics=6"
