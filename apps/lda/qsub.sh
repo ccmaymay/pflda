54caa628777bb861e77b399632bb0d908ef3b019
Chandler May <cjmay@jhu.edu>
2014-02-09 21:06:09 -0500

    fix typo in qsub infrastructure

--- apps/lda/qsub.sh
+++ apps/lda/qsub.sh
@@ -3,7 +3,7 @@
 dataset_path="$1"
 dataset_name="$2"
 experiment="$3"
-d="$experiment/$dataset"
+d="$experiment/$dataset_name"
 
 mkdir -p "$d"
 qsub -o "$d/\$TASK_ID.log" $4 run_lda.qsub $dataset_path $dataset_name $5
