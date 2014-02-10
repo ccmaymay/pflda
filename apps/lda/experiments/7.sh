ef598bac0dba03147a3e34b06ac78d9c5ed79693
Chandler May <cjmay@jhu.edu>
2014-02-10 00:28:37 -0500

    add 7 (oops)

---
+++ apps/lda/experiments/7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dataset_path=../../data/txt/tng-nonalpha
+bash qsub_tng3.sh $dataset_path 7 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+    "--resample_propagate=1 --init_num_docs=100 --init_num_iters=2000"
