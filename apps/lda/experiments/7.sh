cef6496653beecfabfb0ec7878c1ccc481b853ac
Chandler May <cjmay@jhu.edu>
2014-02-10 09:57:48 -0500

    increase time limits due to resampling and init tuning

--- apps/lda/experiments/7.sh
+++ apps/lda/experiments/7.sh
@@ -2,5 +2,5 @@
 
 dataset_path=../../data/txt/tng-nonalpha
 bash qsub_tng3.sh $dataset_path 7 \
-    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=1:00:00" \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=200M,h_rt=2:00:00" \
     "--resample_propagate=1 --init_num_docs=100 --init_num_iters=2000"
