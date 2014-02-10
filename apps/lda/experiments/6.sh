f349a0ab292ca2aacea96f9c081850883c6b37b9
Chandler May <cjmay@jhu.edu>
2014-02-09 21:53:54 -0500

    fix init num iters in 5 and 6, add 8

--- apps/lda/experiments/6.sh
+++ apps/lda/experiments/6.sh
@@ -3,16 +3,16 @@
 dataset_path=../../data/txt/tng-nonalpha
 bash qsub_tng3.sh $dataset_path 6-rs10k-rss10 \
     "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
-    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=10"
+    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=10 --init_num_iters=2000"
 bash qsub_tng3.sh $dataset_path 6-rs10k-rss30 \
     "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
-    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=30"
+    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=30 --init_num_iters=2000"
 bash qsub_tng3.sh $dataset_path 6-rs10k-rss100 \
     "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
-    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=100"
+    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=100 --init_num_iters=2000"
 bash qsub_tng3.sh $dataset_path 6-rs10k-rss300 \
     "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
-    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=300"
+    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=300 --init_num_iters=2000"
 bash qsub_tng3.sh $dataset_path 6-rs10k-rss1k \
     "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
-    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=1000"
+    "--reservoir_size=1000 --init_num_docs=100 --rejuv_sample_size=1000 --init_num_iters=2000"
