27060e97138f816696b960ef5790be3e23ef7c69
Chandler May <cjmay@jhu.edu>
2014-02-15 23:12:46 -0500

    alpha, beta experiments; fix 6 reservoir size

---
+++ apps/lda/experiments/16.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dataset_path=../../data/txt/tng-noheader-nowalls-nonalpha
+bash qsub_tng3.sh $dataset_path 16-rs1k-b0.001 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
+    "--reservoir_size=1000 --init_num_docs=100 --init_num_iters=2000 --beta=0.001"
+bash qsub_tng3.sh $dataset_path 16-rs1k-b0.01 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
+    "--reservoir_size=1000 --init_num_docs=100 --init_num_iters=2000 --beta=0.01"
+bash qsub_tng3.sh $dataset_path 16-rs1k-b0.1 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
+    "--reservoir_size=1000 --init_num_docs=100 --init_num_iters=2000 --beta=0.1"
+bash qsub_tng3.sh $dataset_path 16-rs1k-b1 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
+    "--reservoir_size=1000 --init_num_docs=100 --init_num_iters=2000 --beta=1"
+bash qsub_tng3.sh $dataset_path 16-rs1k-b10 \
+    "-q text.q -tc 2 -l num_proc=1,mem_free=400M,h_rt=2:00:00" \
+    "--reservoir_size=1000 --init_num_docs=100 --init_num_iters=2000 --beta=10"
