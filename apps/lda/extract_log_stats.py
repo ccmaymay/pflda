706a1f4cbb0c4378bf69ccb1ae0617afbeb1a27e
Chandler May <chandler.j.may@gmail.com>
2014-03-06 07:33:32 -0500

    doc -> iter in extract_log_stats.py

--- apps/lda/extract_log_stats.py
+++ apps/lda/extract_log_stats.py
@@ -6,9 +6,9 @@ import sys
 import re
 
 
-DEFAULT_ITER_KEY = 'doc'
+DEFAULT_ITER_KEY = 'iter'
 
-PER_DOC_STAT_NAMES = (
+PER_ITER_STAT_NAMES = (
     'num words',
     'init in-sample nmi',
     'in-sample nmi',
@@ -18,7 +18,7 @@ PER_DOC_STAT_NAMES = (
     'out-of-sample coherence',
 )
 
-PER_DOC_COUNT_NAMES = (
+PER_ITER_COUNT_NAMES = (
     'resampling',
 )
 
@@ -29,24 +29,24 @@ def normalize_stat_name(name):
     return BADNESS_RE.subn('_', name.lower())[0]
 
 
-def dict_key(doc_num, key):
-    return (normalize_stat_name(key), doc_num)
+def dict_key(iter_num, key):
+    return (normalize_stat_name(key), iter_num)
 
 
-def dict_stat_get(d, doc_num, key):
-    return d.get(dict_key(doc_num, key), 'NA')
+def dict_stat_get(d, iter_num, key):
+    return d.get(dict_key(iter_num, key), 'NA')
 
 
-def dict_count_get(d, doc_num, key):
-    return d.get(dict_key(doc_num, key), 0)
+def dict_count_get(d, iter_num, key):
+    return d.get(dict_key(iter_num, key), 0)
 
 
-def dict_set(d, doc_num, key, val):
-    d[dict_key(doc_num, key)] = val
+def dict_set(d, iter_num, key, val):
+    d[dict_key(iter_num, key)] = val
 
 
-def dict_increment(d, doc_num, key):
-    k = dict_key(doc_num, key)
+def dict_increment(d, iter_num, key):
+    k = dict_key(iter_num, key)
     if k in d:
         d[k] += 1
     else:
@@ -63,37 +63,37 @@ def process_logs(experiment_path, iter_key=None):
     for dataset_entry in os.listdir(experiment_path):
         dataset_path = os.path.join(experiment_path, dataset_entry)
         if os.path.isdir(dataset_path):
-            doc_num_bound = 0
-            per_doc_stats_list = []
+            iter_num_bound = 0
+            per_iter_stats_list = []
             entries = os.listdir(dataset_path)
             entries.sort()
 
             for entry in entries:
                 path = os.path.join(dataset_path, entry)
                 if os.path.isfile(path) and not entry.startswith('.'):
-                    (last_doc_num, per_doc_stats) = parse_log(path, iter_key)
-                    if last_doc_num is not None and per_doc_stats:
-                        doc_num_bound = max(last_doc_num + 1, doc_num_bound)
-                        per_doc_stats_list.append(per_doc_stats)
+                    (last_iter_num, per_iter_stats) = parse_log(path, iter_key)
+                    if last_iter_num is not None and per_iter_stats:
+                        iter_num_bound = max(last_iter_num + 1, iter_num_bound)
+                        per_iter_stats_list.append(per_iter_stats)
 
-            for stat_name in PER_DOC_STAT_NAMES:
+            for stat_name in PER_ITER_STAT_NAMES:
                 dataset_tab_path = dataset_path + '_%s.tab' % normalize_stat_name(stat_name)
                 with open(dataset_tab_path, 'w') as f:
-                    f.write('\t'.join(['run.%d' % i for i in range(len(per_doc_stats_list))]) + '\n')
-                    for doc_num in range(doc_num_bound):
-                        f.write('\t'.join([str(dict_stat_get(per_doc_stats, doc_num, stat_name)) for per_doc_stats in per_doc_stats_list]) + '\n')
+                    f.write('\t'.join(['run.%d' % i for i in range(len(per_iter_stats_list))]) + '\n')
+                    for iter_num in range(iter_num_bound):
+                        f.write('\t'.join([str(dict_stat_get(per_iter_stats, iter_num, stat_name)) for per_iter_stats in per_iter_stats_list]) + '\n')
 
-            for count_name in PER_DOC_COUNT_NAMES:
+            for count_name in PER_ITER_COUNT_NAMES:
                 dataset_tab_path = dataset_path + '_%s.tab' % normalize_stat_name(count_name)
                 with open(dataset_tab_path, 'w') as f:
-                    f.write('\t'.join(['run.%d' % i for i in range(len(per_doc_stats_list))]) + '\n')
-                    for doc_num in range(doc_num_bound):
-                        f.write('\t'.join([str(dict_count_get(per_doc_stats, doc_num, count_name)) for per_doc_stats in per_doc_stats_list]) + '\n')
+                    f.write('\t'.join(['run.%d' % i for i in range(len(per_iter_stats_list))]) + '\n')
+                    for iter_num in range(iter_num_bound):
+                        f.write('\t'.join([str(dict_count_get(per_iter_stats, iter_num, count_name)) for per_iter_stats in per_iter_stats_list]) + '\n')
 
 
 def parse_log(log_filename, iter_key):
-    doc_num = None
-    per_doc_stats = dict()
+    iter_num = None
+    per_iter_stats = dict()
 
     with open(log_filename) as f:
         for line in f:
@@ -106,13 +106,13 @@ def parse_log(log_filename, iter_key):
                 val = None
 
             if key == iter_key:
-                doc_num = int(val)
-            elif key in PER_DOC_STAT_NAMES:
-                dict_set(per_doc_stats, doc_num, key, val)
-            elif key in PER_DOC_COUNT_NAMES:
-                dict_increment(per_doc_stats, doc_num, key)
+                iter_num = int(val)
+            elif key in PER_ITER_STAT_NAMES:
+                dict_set(per_iter_stats, iter_num, key, val)
+            elif key in PER_ITER_COUNT_NAMES:
+                dict_increment(per_iter_stats, iter_num, key)
 
-    return (doc_num, per_doc_stats)
+    return (iter_num, per_iter_stats)
 
 
 if __name__ == '__main__':
