0f68c2e6b315657ce2781063108d99613f5bde70
Chandler May <chandler.j.may@gmail.com>
2014-03-16 07:23:48 -0400

    fix demo scripts

--- src/proj/bloomfilter_demo/demo.sh
+++ src/proj/bloomfilter_demo/demo.sh
@@ -3,11 +3,11 @@
 BF_FILE=demo.dat
 DATA_FILE=../../../data/txt/dog.txt
 
-python -m bf.py read $DATA_FILE $BF_FILE
-python -m bf.py query $BF_FILE 'no'
-python -m bf.py query $BF_FILE 'dog bark'
-python -m bf.py query $BF_FILE 'cat bark'
-python -m bf.py query $BF_FILE 'dog clothes'
-python -m bf.py query $BF_FILE 'cat clothes'
-python -m bf.py query $BF_FILE 'dog everrrr'
-python -m bf.py query $BF_FILE 'cat everrrr'
+python -m bf read $DATA_FILE $BF_FILE
+python -m bf query $BF_FILE 'no'
+python -m bf query $BF_FILE 'dog bark'
+python -m bf query $BF_FILE 'cat bark'
+python -m bf query $BF_FILE 'dog clothes'
+python -m bf query $BF_FILE 'cat clothes'
+python -m bf query $BF_FILE 'dog everrrr'
+python -m bf query $BF_FILE 'cat everrrr'
--- src/proj/reservoirsampler_demo/demo.sh
+++ src/proj/reservoirsampler_demo/demo.sh
@@ -4,5 +4,5 @@ RS_FILE=demo.dat
 RS_VALUES_FILE=demo_values.dat
 DATA_FILE=../../../data/txt/dog.txt
 
-python -m rs.py read $DATA_FILE $RS_FILE $RS_VALUES_FILE
-python -m rs.py sample $RS_FILE $RS_VALUES_FILE
+python -m rs read $DATA_FILE $RS_FILE $RS_VALUES_FILE
+python -m rs sample $RS_FILE $RS_VALUES_FILE
