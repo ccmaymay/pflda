9e6c1cf6149f144e6367b39460d5ea0de0616e8f
Chandler May <chandler.j.may@gmail.com>
2014-05-13 23:42:58 -0400

    add script to make dataset from a file (one doc per line) in bglda

---
+++ src/proj/bglda/dataset_from_file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TRAIN_FRAC=0.6
+
+filename="$1"
+dataset_path="$2"
+
+temp_data=`mktemp dataset_from_file.XXXXXX`
+
+mkdir -p "$dataset_path/train" "$dataset_path/test"
+
+sed 's/^/X Y /' "$filename" | shuf > "$temp_data"
+num_docs=`cat "$temp_data" | wc -l`
+python_calc='print int('"$TRAIN_FRAC"' * '"$num_docs"')'
+num_train_docs=`python -c "$python_calc"`
+num_test_docs=$(($num_docs - $num_train_docs))
+
+raw_train_path="$dataset_path/train/all"
+head -n "$num_train_docs" "$temp_data" > "$raw_train_path"
+gzip "$raw_train_path"
+raw_test_path="$dataset_path/test/all"
+tail -n "$num_test_docs" "$temp_data" > "$raw_test_path"
+gzip "$raw_test_path"
+
+rm -f "$temp_data"
