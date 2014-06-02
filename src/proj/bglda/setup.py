d8f0c75f44977153d756b13d063ddacd4f1a7d14
Chandler May <cjmay4754@gmail.com>
2014-06-02 11:05:12 -0400

    refactor bglda setup to put required modules in a package, rename bglda.pyx to core.pyx

--- src/proj/bglda/setup.py
+++ src/proj/bglda/setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 import numpy
 
 ext_modules = [
-    Extension("bglda", ["bglda.pyx"],
+    Extension("bglda.core", ["core.pyx"],
         include_dirs=[numpy.get_include(), '../../pylowl', '../../lowl'],
         library_dirs=['../../pylowl', '../../lowl'],
     ),
@@ -17,8 +17,10 @@ def main():
         description='Batch Gibbs Sampler for LDA',
         url='https://gitlab.hltcoe.jhu.edu/klevin/littleowl',
         cmdclass={'build_ext': build_ext},
+        package_dir = {'bglda': ''},
+        packages=['bglda'],
         ext_modules=ext_modules,
-        py_modules=['data'],
+        py_modules=['bglda.data'],
     )
 
 if __name__ == '__main__':
