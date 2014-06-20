249dc20d3777b8de29ea08bae24573aaa1da4a21
Chandler May <cjmay4754@gmail.com>
2014-06-19 21:44:19 -0400

    tweak qsub to set up for us

--- src/pylowl/proj/brightside/grid.py
+++ src/pylowl/proj/brightside/grid.py
@@ -6,8 +6,8 @@ import itertools as it
 
 
 TRUNC_LEVELS = (
-    '1,7,5,3',
-    #'1,7,5',
+    #'1,7,5,3',
+    '1,7,5',
     #'1,35',
 )
 
@@ -43,28 +43,21 @@ IOTA_LEVELS = (
     '1.0',
 )
 
+BATCHSIZE_LEVELS = (
+    '100',
+)
+
 
 def main():
     cells = it.product(TRUNC_LEVELS, ALPHA_LEVELS, BETA_LEVELS,
-                       GAMMA1_LEVELS, GAMMA2_LEVELS, KAPPA_LEVELS, IOTA_LEVELS)
-    for (trunc, alpha, beta, gamma1, gamma2, kappa, iota) in cells:
-        args = ['qsub', 'run_m0.qsub', trunc, '--log_level=INFO', '--burn_in_samples=1000', '--test_samples=1000', '--save_model', '--gamma1', gamma1, '--gamma2', gamma2, '--alpha', alpha, '--beta', beta, '--kappa', kappa, '--iota', iota]
+                       GAMMA1_LEVELS, GAMMA2_LEVELS, KAPPA_LEVELS, IOTA_LEVELS,
+                       BATCHSIZE_LEVELS)
+    for (trunc, alpha, beta, gamma1, gamma2, kappa, iota, batchsize) in cells:
+        args = ['qsub', 'run_m0.qsub', trunc, '--init_samples=1000', '--test_samples=100', '--save_model', '--batchsize', batchsize,'--gamma1', gamma1, '--gamma2', gamma2, '--alpha', alpha, '--beta', beta, '--kappa', kappa, '--iota', iota]
         print ' '.join(args)
         subprocess.call(args)
 
 
 if __name__ == '__main__':
     import sys
-
-    args = []
-    params = dict()
-    for token in sys.argv[1:]:
-        eq_pos = token.find('=')
-        if token.startswith('--') and eq_pos >= 0:
-            k = token[len('--'):eq_pos]
-            v = token[(eq_pos+1):len(token)]
-            params[k] = v
-        else:
-            args.append(token)
-
-    main(*args, **params)
+    main(*sys.argv[1:])
