5396e96d7245e4326f4664da4ba2ab79a1c4d0c0
Chandler May <chandler.j.may@gmail.com>
2014-06-27 08:39:27 -0400

    partial update of generate_d3_topic_graph: added argparse, abstracted a few bits, but not sure how to do word selection/ordering under multiple weighting schemes

--- src/pylowl/proj/brightside/postproc/generate_d3_topic_graph.py
+++ src/pylowl/proj/brightside/postproc/generate_d3_topic_graph.py
@@ -6,7 +6,51 @@ import json
 from pylowl.proj.brightside.utils import take, load_vocab, tree_index_m, tree_index_b, tree_iter, tree_index
 
 
-NUM_TYPES_PER_TOPIC = 10
+DEFAULT_NUM_WORDS_PER_TOPIC = 10
+
+
+def main():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.set_defaults(
+        lambda_ss=None,
+        Elogpi=None,
+        logEpi=None,
+        Elogtheta=None,
+        logEtheta=None,
+        num_words_per_topic=DEFAULT_NUM_WORDS_PER_TOPIC,
+    )
+    parser.add_argument('trunc_csv', type=str,
+                        help='comma-separated list of truncations (per level)')
+    parser.add_argument('vocab_path', type=str,
+                        help='vocab file path')
+    parser.add_argument('output_path', type=str,
+                        help='output file path')
+    parser.add_argument('--lambda_ss', type=str,
+                        help='lambda_ss file path')
+    parser.add_argument('--Elogpi', type=str,
+                        help='Elogpi file path')
+    parser.add_argument('--logEpi', type=str,
+                        help='logEpi file path')
+    parser.add_argument('--Elogtheta', type=str,
+                        help='Elogtheta file path')
+    parser.add_argument('--logEtheta', type=str,
+                        help='logEtheta file path')
+    parser.add_argument('--num_words_per_topic', type=int,
+                        help='number of words to write per topic')
+
+    args = parser.parse_args()
+    generate_d3_topic_graph(
+        args.trunc_csv,
+        args.vocab_path,
+        args.output_path,
+        lambda_ss_filename=args.lambda_ss,
+        Elogpi_filename=args.Elogpi,
+        logEpi_filename=args.logEpi,
+        Elogtheta_filename=args.Elogtheta,
+        logEtheta_filename=args.logEtheta,
+        num_words_per_topic=args.num_words_per_topic
+    )
 
 
 def sorted_topic_word_weight_lists(input_filename, vocab):
@@ -19,27 +63,40 @@ def sorted_topic_word_weight_lists(input_filename, vocab):
                 reverse=True)
 
 
-def main(trunc_csv, vocab_filename, input_filename, output_filename):
+def generate_d3_topic_graph(trunc_csv, vocab_filename, output_filename,
+        lambda_ss_filename=None,
+        Elogpi_filename=None,
+        logEpi_filename=None,
+        Elogtheta_filename=None,
+        logEtheta_filename=None,
+        num_words_per_topic=DEFAULT_NUM_WORDS_PER_TOPIC):
+
     vocab = load_vocab(vocab_filename)
 
     node_topics = []
     graph = {}
 
+    # TODO how to select words/sort?!
     for ww_list in sorted_topic_word_weight_lists(input_filename, vocab):
-        label = []
-        topic_weight = sum(ww[1] for ww in ww_list)
-        for (word, weight) in take(ww_list, NUM_TYPES_PER_TOPIC):
-            label.append((word, weight))
-        node_topics.append((label, topic_weight))
+        words = []
+        for (word, weight) in take(ww_list, num_words_per_topic):
+            words.append({
+                'word': word,
+                'weight': weight
+            })
+        node_topics.append({
+            'words': words,
+            'weight': 0 # TODO
+            'children': []
+        })
 
     trunc = [int(t) for t in trunc_csv.split(',')]
     m = tree_index_m(trunc)
     b = tree_index_b(trunc)
     for node in tree_iter(trunc):
         idx = tree_index(node, m, b)
-        node_dict = {'words': [{'word': p[0], 'weight': p[1]} for p in node_topics[idx][0]], 'children': [], 'weight': node_topics[idx][1]}
         parent = node[:-1]
-        graph[node] = node_dict
+        graph[node] = node_topics[idx]
         if parent:
             graph[parent]['children'].append(node_dict)
 
@@ -48,17 +105,4 @@ def main(trunc_csv, vocab_filename, input_filename, output_filename):
 
 
 if __name__ == '__main__':
-    import sys
-
-    args = []
-    params = dict()
-    for token in sys.argv[1:]:
-        eq_pos = token.find('=')
-        if token.startswith('--') and eq_pos >= 0:
-            k = token[len('--'):eq_pos]
-            v = token[(eq_pos+1):len(token)]
-            params[k] = v
-        else:
-            args.append(token)
-
-    main(*args, **params)
+    main()
