f05b59259f48547047e0f93515d031c3d80e60df
Chandler May <chandler.j.may@gmail.com>
2014-07-03 14:57:40 -0400

    use own raises (not nose)

--- src/pylowl/core_test.py
+++ src/pylowl/core_test.py
@@ -2,7 +2,22 @@ from pylowl.core import *
 from numpy.testing import assert_almost_equal
 
 
-def _chisq(expected, observed):
+def raises(f, ex_type):
+    '''
+    Return true iff f, when called, raises an exception of type ex_type
+    '''
+
+    try:
+        f()
+    except ex_type:
+        return True
+    except:
+        return False
+    else:
+        return False
+
+
+def chisq(expected, observed):
     """
     Compute chi-squared statistic for the given sequences of expected
     and observed counts.
@@ -15,25 +30,25 @@ def _chisq(expected, observed):
 
 
 def chisq_1_test():
-    assert_almost_equal(_chisq([3], [3]), 0.0)
+    assert_almost_equal(chisq([3], [3]), 0.0)
 
 def chisq_2_test():
-    assert_almost_equal(_chisq([1], [3]), 4.0)
+    assert_almost_equal(chisq([1], [3]), 4.0)
 
 def chisq_3_test():
-    assert_almost_equal(_chisq([3], [1]), 4.0 / 3.0)
+    assert_almost_equal(chisq([3], [1]), 4.0 / 3.0)
 
 def chisq_4_test():
-    assert_almost_equal(_chisq([1, 3, 5], [1, 3, 5]), 0.0)
+    assert_almost_equal(chisq([1, 3, 5], [1, 3, 5]), 0.0)
 
 def chisq_5_test():
-    assert_almost_equal(_chisq([5, 3, 1], [1, 3, 5]), 16.0 / 5.0 + 16.0)
+    assert_almost_equal(chisq([5, 3, 1], [1, 3, 5]), 16.0 / 5.0 + 16.0)
 
 def chisq_6_test():
-    assert_almost_equal(_chisq([1, 3, 5], [5, 3, 1]), 16.0 / 5.0 + 16.0)
+    assert_almost_equal(chisq([1, 3, 5], [5, 3, 1]), 16.0 / 5.0 + 16.0)
 
 def chisq_7_test():
-    assert_almost_equal(_chisq([2, 4, 6], [1, 3, 5]), 0.25 + 0.5 + 1.0 / 6.0)
+    assert_almost_equal(chisq([2, 4, 6], [1, 3, 5]), 0.25 + 0.5 + 1.0 / 6.0)
 
 def srandom_test():
     srandom(0)
