fix nu normalization

src/pylowl/proj/brightside/model.py
@@ -282,13 +282,15 @@ class model(object):
         Elogchi = self.compute_subtree_Elogchi(subtree_leaves, ab)
         for node in self.tree_iter(subtree_leaves):
             idx = self.tree_index(node)
+            node_level = self.node_level(node)
             for p in it.chain((node,), self.node_ancestors(node)):
                 p_idx = self.tree_index(p)
                 p_level = self.node_level(p)
                 log_nu[idx,:,p_level] = Elogchi[p_idx] + xi[idx] * np.repeat(Elogprobw_doc[p_idx,:], doc.counts)
+            for n in xrange(doc.total):
+                (log_nu[idx,n,:node_level+1], log_norm) = utils.log_normalize(log_nu[idx,n,:node_level+1])
 
-        (log_nu[:,:], log_norm) = utils.log_normalize(log_nu)
-        nu[:,:] = np.exp(log_nu)
+        nu[:] = np.exp(log_nu)
 
     def update_ab(self, subtree_leaves, nu_sums, ab):
         ab[0] = self.m_gamma1 + nu_sums

src/pylowl/proj/brightside/utils.py
@@ -174,27 +174,15 @@ def log_normalize(v):
     '''
     Return pair: normalization of v and the normalizer.  (v is assumed
     to be in log space; the normalization r satisfies sum(exp(r)) = 1.
-    The normalizer is also represented in log space.)  v can also be a
-    matrix, in which case the normalization is performed column-wise
-    (each row r satisfies sum(exp(r)) = 1) and the normalizer is a
-    vector containing the normalizer for each row.
+    The normalizer is also represented in log space.)
     '''
 
     log_max = 100.0
-    if len(v.shape) == 1:
-        max_val = np.max(v)
-        log_shift = log_max - np.log(len(v) + 1.0) - max_val
-        tot = np.sum(np.exp(v + log_shift))
-        log_norm = np.log(tot) - log_shift
-        v = v - log_norm
-    else:
-        max_val = np.max(v, 1)
-        log_shift = log_max - np.log(v.shape[1] + 1.0) - max_val
-        tot = np.sum(np.exp(v + log_shift[:, np.newaxis]), 1)
-
-        log_norm = np.log(tot) - log_shift
-        v = v - log_norm[:, np.newaxis]
-
+    max_val = np.max(v)
+    log_shift = log_max - np.log(sum(v.shape) + 1.0) - max_val
+    tot = np.sum(np.exp(v + log_shift))
+    log_norm = np.log(tot) - log_shift
+    v = v - log_norm
     return (v, log_norm)
 
 