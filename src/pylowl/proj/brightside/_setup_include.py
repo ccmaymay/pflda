3ee12dbd3dfa5d50f9f59dbc8b8e4dfe739bf6ff
Chandler May <chandler.j.may@gmail.com>
2014-07-07 09:07:20 -0400

    add commented-out cython sketch to brightside setup_include

--- src/pylowl/proj/brightside/_setup_include.py
+++ src/pylowl/proj/brightside/_setup_include.py
@@ -2,5 +2,11 @@ from distutils.extension import Extension
 
 def select(packages, ext_modules, scripts):
     packages.append('pylowl.proj.brightside')
+    #ext_modules.append(
+    #    Extension('pylowl.proj.brightside.core',
+    #        ['src/pylowl/proj/brightside/core.pyx'],
+    #        include_dirs=['src', 'src/lowl', numpy.get_include()],
+    #        extra_compile_args=['-std=gnu99', '-O0', '-g'],
+    #    ))
     packages.append('pylowl.proj.brightside.preproc')
     packages.append('pylowl.proj.brightside.postproc')
