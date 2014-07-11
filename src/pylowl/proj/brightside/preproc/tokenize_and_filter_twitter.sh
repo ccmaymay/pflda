f5eb680d762c6125fee252f3a7b50639cb0414d3
Chandler May <cjmay4754@gmail.com>
2014-07-11 01:12:16 -0400

    expand tokenize_and_filter_twitter idf bounds

--- src/pylowl/proj/brightside/preproc/tokenize_and_filter_twitter.sh
+++ src/pylowl/proj/brightside/preproc/tokenize_and_filter_twitter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m pylowl.proj.brightside.preproc.tokenize_and_filter ~/ds2.concrete.split/{train,test}/'*' ~/ds2.concrete.split.tokenized.filtered/{train,test,vocab} --lowercase --idf_lb=0.01 --idf_ub=0.2 --char_filter_pattern '\W+(?=\w)' --char_filter_pattern '(?<=\w)\W+' --token_filter_pattern '@\w+$'
+python -m pylowl.proj.brightside.preproc.tokenize_and_filter ~/ds2.concrete.split/{train,test}/'*' ~/ds2.concrete.split.tokenized.filtered/{train,test,vocab} --lowercase --idf_lb=0.005 --idf_ub=0.2 --char_filter_pattern '\W+(?=\w)' --char_filter_pattern '(?<=\w)\W+' --token_filter_pattern '@\w+$'
