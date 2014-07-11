2c2ebeff3dd61bc78cede2e69f7747e36758eead
Chandler May <cjmay4754@gmail.com>
2014-07-11 18:27:11 -0400

    revert accidental bound expansion in twitter filter...

--- src/pylowl/proj/brightside/preproc/tokenize_and_filter_twitter.sh
+++ src/pylowl/proj/brightside/preproc/tokenize_and_filter_twitter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m pylowl.proj.brightside.preproc.tokenize_and_filter ~/ds2.concrete.split/{train,test}/'*' ~/ds2.concrete.split.tokenized.filtered/{train,test,vocab} --lowercase --idf_lb=0.0005 --idf_ub=0.2 --char_filter_pattern '\W+(?=\w)' --char_filter_pattern '(?<=\w)\W+' --token_filter_pattern '@\w+' --token_filter_pattern '.*USER_[a-z0-9]{7}'
+python -m pylowl.proj.brightside.preproc.tokenize_and_filter ~/ds2.concrete.split/{train,test}/'*' ~/ds2.concrete.split.tokenized.filtered/{train,test,vocab} --lowercase --idf_lb=0.0005 --idf_ub=0.1 --char_filter_pattern '\W+(?=\w)' --char_filter_pattern '(?<=\w)\W+' --token_filter_pattern '@\w+' --token_filter_pattern '.*USER_[a-z0-9]{7}'
