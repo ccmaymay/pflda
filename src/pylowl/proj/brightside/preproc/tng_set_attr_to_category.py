f28d60f5f3cf53453d28fcf1730dedf3a3d97aa9
Chandler May <cjmay4754@gmail.com>
2014-07-20 09:59:11 -0400

    fix tng_set_attr_to_category (comm keyValueMap)

--- src/pylowl/proj/brightside/preproc/tng_set_attr_to_category.py
+++ src/pylowl/proj/brightside/preproc/tng_set_attr_to_category.py
@@ -6,28 +6,24 @@ from pylowl.proj.brightside.utils import nested_file_paths
 from pylowl.proj.brightside.corpus import update_concrete_comms
 
 
-DEFAULT_ATTR = 'user'
-
-
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.set_defaults(attr=DEFAULT_ATTR)
     parser.add_argument('data_dir', type=str,
                         help='data directory path')
-    parser.add_argument('--attr', type=str,
+    parser.add_argument('attr', type=str,
                         help='name of attribute to set')
 
     args = parser.parse_args()
-    tng_set_attr_to_category(args.data_dir, attr=args.attr)
+    tng_set_attr_to_category(args.data_dir, args.attr)
 
 
 def set_attr(comm, attr):
     category = os.path.basename(os.path.dirname(self.id))
-    comm.attrs[attr] = category
+    comm.keyValueMap[attr] = category
 
 
-def tng_set_attr_to_category(data_dir, attr=DEFAULT_ATTR):
+def tng_set_attr_to_category(data_dir, attr):
     update_concrete_comms(nested_file_paths(data_dir),
                           lambda c: set_attr(c, attr))
 
