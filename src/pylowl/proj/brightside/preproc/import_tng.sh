52c475a7962f3e0750ac95adf3cb20334b9ebbef
Chandler May <cjmay4754@gmail.com>
2014-07-20 10:07:37 -0400

    tokenize_and_filter to import_tng

--- src/pylowl/proj/brightside/preproc/import_tng.sh
+++ src/pylowl/proj/brightside/preproc/import_tng.sh
@@ -8,7 +8,20 @@
 
 for t in train test
 do
-    python -m pylowl.proj.brightside.preproc.tng_to_concrete ~/20news-bydate-$t data/txt/tng.concrete.orig.split/$t --remove_non_ascii --remove_emails --remove_walls --remove_first_paragraph --remove_email_headers --remove_email_history --remove_writes_lines
+    python -m pylowl.proj.brightside.preproc.tng_to_concrete \
+        $HOME/20news-bydate-$t \
+        data/txt/tng.concrete.orig.split/$t \
+        --remove_non_ascii \
+        --remove_emails \
+        --remove_walls \
+        --remove_first_paragraph \
+        --remove_email_headers \
+        --remove_email_history \
+        --remove_writes_lines
 done
 python -m pylowl.proj.brightside.preproc.tng_set_class_to_has_gpe data/txt/tng.concrete.orig.split
 python -m pylowl.proj.brightside.preproc.tng_set_attr_to_category data/txt/tng.concrete.orig.split user
+python -m pylowl.proj.brightside.preproc.tokenize_and_filter \
+    data/txt/tng.orig.split/{train,test} \
+    data/txt/tng/{train,test,vocab} \
+    --idf_lb=0.001 --idf_ub=0.05 --lowercase --split_pattern '[^a-zA-Z]+'
