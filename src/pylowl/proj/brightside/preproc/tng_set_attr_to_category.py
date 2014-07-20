fdf7d384d181ff085d6d899940a1436f05b16a02
Chandler May <cjmay4754@gmail.com>
2014-07-20 06:28:35 -0400

    abstract tng set attr to category

--- src/pylowl/proj/brightside/preproc/tng_set_attr_to_category.py
+++ src/pylowl/proj/brightside/preproc/tng_set_attr_to_category.py
@@ -6,23 +6,30 @@ from utils import nested_file_paths
 from pylowl.proj.brightside.corpus import update_concrete_comms
 
 
+DEFAULT_ATTR = 'user'
+
+
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.set_defaults(attr=DEFAULT_ATTR)
     parser.add_argument('data_dir', type=str,
                         help='data directory path')
+    parser.add_argument('--attr', type=str,
+                        help='name of attribute to set')
 
     args = parser.parse_args()
-    tng_set_user_to_category(args.data_dir)
+    tng_set_user_to_category(args.data_dir, attr=args.attr)
 
 
-def set_user(comm):
+def set_user(comm, attr):
     category = os.path.basename(os.path.dirname(self.id))
-    comm.attrs['user'] = category
+    comm.attrs[attr] = category
 
 
-def tng_set_user_to_category(data_dir):
-    update_concrete_comms(nested_file_paths(data_dir))
+def tng_set_user_to_category(data_dir, attr=DEFAULT_ATTR):
+    update_concrete_comms(nested_file_paths(data_dir),
+                          lambda c: set_user(c, attr))
 
 
 if __name__ == '__main__':
