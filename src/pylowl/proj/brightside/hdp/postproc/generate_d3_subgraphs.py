a0f3a0572b5261928d34505f58af4042fc851769
Chandler May <cjmay4754@gmail.com>
2014-07-25 09:48:49 -0400

    bugfixes, variable renames in subgraph port from shdp to hdp

--- src/pylowl/proj/brightside/hdp/postproc/generate_d3_subgraphs.py
+++ src/pylowl/proj/brightside/hdp/postproc/generate_d3_subgraphs.py
@@ -27,12 +27,12 @@ def generate_d3_subgraphs(result_dir, output_filename):
     options = load_options(os.path.join(result_dir, 'options'))
     K = int(options['K'])
     L = int(options['L'])
-    sublist_filename = os.path.join(result_dir, 'final.sublist')
-    lambda_ss_filename = os.path.join(result_dir, 'final.sublist_lambda_ss')
-    Elogpi_filename = os.path.join(result_dir, 'final.sublist_Elogpi')
-    logEpi_filename = os.path.join(result_dir, 'final.sublist_logEpi')
+    sublist_filename = os.path.join(result_dir, 'sublist')
+    lambda_ss_filename = os.path.join(result_dir, 'sublist_lambda_ss')
+    Elogpi_filename = os.path.join(result_dir, 'sublist_Elogpi')
+    logEpi_filename = os.path.join(result_dir, 'sublist_logEpi')
 
-    sublist_dicts_per_class = {}
+    sublist_dicts_per_doc = {}
 
     with open(sublist_filename) as sublist_f, \
          open(Elogpi_filename) as Elogpi_f, \
@@ -41,7 +41,7 @@ def generate_d3_subgraphs(result_dir, output_filename):
         for (sublist_line, stat_lines) in it.izip(sublist_f, it.izip(
                 Elogpi_f, logEpi_f, lambda_ss_f)):
             pieces = sublist_line.strip().split()
-            klass = pieces[0]
+            doc_id = pieces[0]
             phi = [float(z) for z in pieces[1:]]
 
             sublist_dicts = [None] * L
@@ -55,25 +55,25 @@ def generate_d3_subgraphs(result_dir, output_filename):
                     ('Elogpi', 'logEpi', 'lambda_ss'),
                     stat_lines):
                 stat_pieces = stat_line.strip().split()
-                if klass != stat_pieces[0]:
-                    raise Exception('classs do not match: %s and %s'
-                                    % (klass, stat_pieces[0]))
+                if doc_id != stat_pieces[0]:
+                    raise Exception('doc ids do not match: %s and %s'
+                                    % (doc_id, stat_pieces[0]))
                 weights = [float(w) for w in stat_pieces[1:]]
                 for j in xrange(L):
                     sublist_dicts[j][stat_name] = weights[j]
 
-            sublist_dicts_per_class[klass] = sublist_dicts
+            sublist_dicts_per_doc[doc_id] = sublist_dicts
 
     json_data = []
 
-    for (klass, sublist_dicts) in sublist_dicts_per_class.items():
+    for (doc_id, sublist_dicts) in sublist_dicts_per_doc.items():
         lambda_ss_sum = 0
         for j in xrange(L):
             if j > 0:
                 sublist_dicts[j-1]['children'].append(sublist_dicts[j])
             lambda_ss_sum += sublist_dicts[j]['lambda_ss']
         json_data.append({
-            'class': klass,
+            'doc_id': doc_id,
             'sublist': sublist_dicts[0],
             'lambda_ss_sum': lambda_ss_sum
         })
