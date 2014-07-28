a23958d2455a0e268eb07949a6404b9c22fe1e2c
Chandler May <cjmay4754@gmail.com>
2014-07-28 17:50:23 -0400

    apply abstractions to run_tng

--- src/pylowl/proj/brightside/m1/run_tng.py
+++ src/pylowl/proj/brightside/m1/run_tng.py
@@ -6,77 +6,52 @@
 #$ -q text.q
 #$ -l num_proc=1,mem_free=2G,h_rt=2:00:00
 
-import sys
-import os
-import tempfile
-import shutil
-import pkg_resources
-from pylowl.proj.brightside.m1.run import run
-from pylowl.proj.brightside.m1.postproc.generate_d3_graph import generate_d3_graph
-from pylowl.proj.brightside.m1.postproc.generate_d3_subgraphs import generate_d3_subgraphs
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    print 'sys.path:'
+    for path in sys.path:
+        print '    %s' % path
+    print
+
+    print 'os.environ:'
+    for (k, v) in os.environ.items():
+        print '    %s: %s' % (k, v)
+    print
 
 
-POSTPROC_PKG = 'pylowl.proj.brightside.m1.postproc'
-BRIGHTSIDE_POSTPROC_PKG = 'pylowl.proj.brightside.postproc'
+    from pylowl.proj.brightside.utils import make_output_dir
+    from pylowl.proj.brightside.m1.run import run
+    from pylowl.proj.brightside.m1.postproc.utils import postprocess
 
-profile = ('--profile' in sys.argv[1:])
+    print 'Creating output directory...'
+    output_dir = make_output_dir('output/pylowl/proj/brightside/m1')
 
-print 'sys.path:'
-for path in sys.path:
-    print '    %s' % path
-print
+    print 'Running stochastic variational inference...'
+    run(trunc='1,5,4',
+        data_dir='data/txt/tng/train',
+        test_data_dir='data/txt/tng/test',
+        test_samples=400,
+        init_samples=400,
+        alpha=1,
+        beta=1,
+        gamma1=1,
+        gamma2=1,
+        iota=64,
+        kappa=0.6,
+        lambda0=0.005,
+        max_time=360,
+        save_model=True,
+        output_dir=output_dir,
+        vocab_path='data/txt/tng/vocab',
+        user_doc_reservoir_capacity=200,
+        user_subtree_selection_interval=50,
+        log_level='DEBUG')
 
-print 'os.environ:'
-for (k, v) in os.environ.items():
-    print '    %s: %s' % (k, v)
-print
+    print 'Postprocessing...'
+    postprocess(output_dir)
 
-TRUNC = '1,5,4'
-DATA_DIR = 'data/txt/tng'
-VOCAB_PATH = os.path.join(DATA_DIR, 'vocab')
-TRAIN_DATA_DIR = os.path.join(DATA_DIR, 'train')
-TEST_DATA_DIR = os.path.join(DATA_DIR, 'test')
-
-print 'Creating output directory...'
-OUTPUT_DIR_BASE = 'output/pylowl/proj/brightside/m1'
-if not os.path.isdir(OUTPUT_DIR_BASE):
-    os.makedirs(OUTPUT_DIR_BASE)
-OUTPUT_DIR = tempfile.mkdtemp(prefix='', suffix='', dir=OUTPUT_DIR_BASE)
-umask = os.umask(0o022) # whatever, python
-os.umask(umask) # set umask back
-os.chmod(OUTPUT_DIR, 0o0755 & ~umask)
-
-print 'Running stochastic variational inference...'
-code = '''run(trunc=TRUNC,
-    data_dir=TRAIN_DATA_DIR,
-    test_data_dir=TEST_DATA_DIR,
-    test_samples=400,
-    init_samples=400,
-    max_time=360,
-    save_model=True,
-    output_dir=OUTPUT_DIR,
-    vocab_path=VOCAB_PATH,
-    user_doc_reservoir_capacity=200,
-    user_subtree_selection_interval=50,
-    log_level='DEBUG')'''.replace('\n', ' ')
-if profile:
-    print 'Profiling...'
-    import cProfile
-    cProfile.run(code, os.path.join(OUTPUT_DIR, 'profile'))
-else:
-    exec code
-
-print 'Generating D3 inputs...'
-generate_d3_graph(OUTPUT_DIR, os.path.join(OUTPUT_DIR, 'graph.json'))
-generate_d3_subgraphs(OUTPUT_DIR, os.path.join(OUTPUT_DIR, 'subgraphs.json'))
-
-print 'Linking visualization code to output directory...'
-for basename in ('subgraphs.html',):
-    shutil.copy(pkg_resources.resource_filename(POSTPROC_PKG, basename),
-        os.path.join(OUTPUT_DIR, basename))
-for basename in ('d3.v3.js', 'core.js', 'graph.html'):
-    shutil.copy(pkg_resources.resource_filename(BRIGHTSIDE_POSTPROC_PKG, basename),
-        os.path.join(OUTPUT_DIR, basename))
-
-print 'Done:'
-print OUTPUT_DIR
+    print 'Done:'
+    print output_dir
--- src/pylowl/proj/brightside/shdp/run_tng.py
+++ src/pylowl/proj/brightside/shdp/run_tng.py
@@ -6,85 +6,49 @@
 #$ -q text.q
 #$ -l num_proc=1,mem_free=2G,h_rt=2:00:00
 
-import sys
-import os
-import re
-import shutil
-import pkg_resources
-import tempfile
-from pylowl.proj.brightside.shdp.run import run
-from pylowl.proj.brightside.shdp.postproc.generate_d3_graph import generate_d3_graph
-from pylowl.proj.brightside.shdp.postproc.generate_d3_subgraphs import generate_d3_subgraphs
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    print 'sys.path:'
+    for path in sys.path:
+        print '    %s' % path
+    print
+
+    print 'os.environ:'
+    for (k, v) in os.environ.items():
+        print '    %s: %s' % (k, v)
+    print
 
 
-POSTPROC_PKG = 'pylowl.proj.brightside.shdp.postproc'
-BRIGHTSIDE_POSTPROC_PKG = 'pylowl.proj.brightside.postproc'
+    from pylowl.proj.brightside.utils import make_output_dir
+    from pylowl.proj.brightside.shdp.run import run
+    from pylowl.proj.brightside.shdp.postproc.utils import postprocess
 
-profile = ('--profile' in sys.argv[1:])
+    print 'Creating output directory...'
+    output_dir = make_output_dir('output/pylowl/proj/brightside/shdp')
 
-print 'sys.path:'
-for path in sys.path:
-    print '    %s' % path
-print
+    print 'Running stochastic variational inference...'
+    run(K=40, J=10, I=5,
+        data_dir='data/txt/tng/train',
+        test_data_dir='data/txt/tng/test',
+        test_samples=400,
+        init_samples=400,
+        alpha=1,
+        beta=1,
+        gamma=1,
+        iota=64,
+        kappa=0.6,
+        lambda0=0.005,
+        max_time=360,
+        save_model=True,
+        output_dir=output_dir,
+        vocab_path='data/txt/tng/vocab',
+        log_level='DEBUG')
 
-print 'os.environ:'
-for (k, v) in os.environ.items():
-    print '    %s: %s' % (k, v)
-print
+    print 'Postprocessing...'
+    postprocess(output_dir)
 
-K = 20
-J = 10
-I = 5
-M = 2
-DATA_DIR = 'data/txt/tng'
-VOCAB_PATH = os.path.join(DATA_DIR, 'vocab')
-TRAIN_DATA_DIR = os.path.join(DATA_DIR, 'train')
-TEST_DATA_DIR = os.path.join(DATA_DIR, 'test')
-
-print 'Creating output directory...'
-OUTPUT_DIR_BASE = 'output/pylowl/proj/brightside/shdp'
-if not os.path.isdir(OUTPUT_DIR_BASE):
-    os.makedirs(OUTPUT_DIR_BASE)
-OUTPUT_DIR = tempfile.mkdtemp(prefix='', suffix='', dir=OUTPUT_DIR_BASE)
-umask = os.umask(0o022) # whatever, python
-os.umask(umask) # set umask back
-os.chmod(OUTPUT_DIR, 0o0755 & ~umask)
-
-print 'Running stochastic variational inference...'
-code = '''run(I=I, J=J, K=K,
-    data_dir=TRAIN_DATA_DIR,
-    test_data_dir=TEST_DATA_DIR,
-    test_samples=300,
-    init_samples=300,
-    alpha=1,
-    beta=1,
-    gamma=1,
-    iota=64,
-    kappa=0.6,
-    lambda0=0.005,
-    max_time=360,
-    save_model=True,
-    output_dir=OUTPUT_DIR,
-    vocab_path=VOCAB_PATH,
-    log_level='DEBUG')'''.replace('\n', ' ')
-if profile:
-    print 'Profiling...'
-    import cProfile
-    cProfile.run(code, os.path.join(OUTPUT_DIR, 'profile'))
-else:
-    exec code
-
-print 'Generating D3 inputs...'
-generate_d3_graph(OUTPUT_DIR, os.path.join(OUTPUT_DIR, 'graph.json'))
-generate_d3_subgraphs(OUTPUT_DIR, os.path.join(OUTPUT_DIR, 'subgraphs.json'))
-
-print 'Linking visualization code to output directory...'
-for basename in ('subgraphs.html',):
-    shutil.copy(pkg_resources.resource_filename(POSTPROC_PKG, basename),
-        os.path.join(OUTPUT_DIR, basename))
-for basename in ('d3.v3.js', 'core.js', 'graph.html'):
-    shutil.copy(pkg_resources.resource_filename(BRIGHTSIDE_POSTPROC_PKG, basename),
-        os.path.join(OUTPUT_DIR, basename))
-
-print 'Done:'
-print OUTPUT_DIR
+    print 'Done:'
+    print output_dir
